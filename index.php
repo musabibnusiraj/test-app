c8a1bbe3a4097aa1993e9e04e5bc0abcbb90c88b
Musab Ibnu Siraj <musab.dot@gmail.com>
2024-10-17 12:06:55 +0530

    log out - fn

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<h1>Assalamu Alaikkum <h1>
+<?php
+
+include __DIR__ . '/config.php';
+include BASE_PATH . '/helpers/AppManager.php';
+
+$sm = AppManager::getSM();
+$username = $sm->getAttribute("username");
+
+if (isset($username)) {
+    header('location: dashboard.php');
+} else {
+    header('location: login.php');
+}
